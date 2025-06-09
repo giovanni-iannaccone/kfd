efabd1e8240c076818abbffb261791e45f8906d0
giovanni-iannaccone <iannacconegiovanni444@gmail.com>
2025-06-09 14:37:52 +0200

    Fix bug

--- src/kfd.c
+++ src/kfd.c
@@ -15,10 +15,10 @@ static void help(const char program_name[]) {
     printf("-j  | --jwt\t\tgenerate a jwt key\n");
     printf("-u  | --uuid\t\tgenerate version 4 uuid\n\n");
 
-    printf("-a {len} | --alphanum {len}\t\tget a random string of len length\n");
+    printf("-a {len} | --alphanum {len}\tget a random string of len length\n");
     printf("-c {len} | --char {len}\t\tget a random letters-only string of len lenght\n");
-    printf("-h {len} | --hex {len}\t\tget a random hex of len length\n");
-    printf("-n {len} | --number {len}\t\tget a random number of len length\n\n");
+    printf("-x {len} | --hex {len}\t\tget a random hex of len length\n");
+    printf("-n {len} | --number {len}\tget a random number of len length\n\n");
 }
 
 static void parse_flags(const int argc, const char *argv[]) {
@@ -31,24 +31,25 @@ static void parse_flags(const int argc, const char *argv[]) {
         } else if (strcmp(argv[i], "-u") == 0 || strcmp(argv[i], "--uuid") == 0 ) {
             str = generate_uuid();
 
-        } else if (strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--alphanum") == 0 && i + 1 < argc) {
+        } else if ((strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--alphanum") == 0) && i + 1 < argc) {
             str = generate_alphanum(atoi(argv[i + 1]));
             i++;
         
-        } else if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--char") == 0 && i + 1 < argc) {
+        } else if ((strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--char") == 0) && i + 1 < argc) {
             str = generate_char(atoi(argv[i + 1]));
             i++;
         
-        } else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--hex") == 0 && i + 1 < argc) {
+        } else if ((strcmp(argv[i], "-x") == 0 || strcmp(argv[i], "--hex") == 0) && i + 1 < argc) {
             str = generate_hex(atoi(argv[i + 1]));
             i++;
 
-        } else if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--number") == 0 && i + 1 < argc) {
+        } else if ((strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--number") == 0) && i + 1 < argc) {
             str = generate_number(atoi(argv[i + 1]));
             i++;
         
         } else {
             help(argv[0]);
+            continue;
         }
     
         printf("%s\n", str);
